21f8413e2160497aa911fc37890c953847f256f7
artemlarchenkov <112898231+artemlarchenkov@users.noreply.github.com>
2025-08-25 22:36:23 +0300

    git init  + работа с конфиг файлом

---
+++ cmd/url-shortener/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"url-shortener/url-shortener/internal/config"
+)
+
+func main() {
+	// TODO: init config: cleanenv
+	cfg := config.MustLoad()
+
+	fmt.Println(cfg)
+
+	// TODO: init logger: slog
+
+	//TODO: init storage: sqlite
+
+	//TODO: init router: chi, "chi render"
+
+	//TODO: run server
+	fmt.Println("Hello!")
+}
